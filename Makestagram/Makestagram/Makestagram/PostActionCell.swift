0fadf32faefd4d877288585a37df614b75293598
LaurenWeiss <lolow123@comcast.net>
2017-06-29 16:48:57 -0700

    Through Step 17 and half of 18
    Through Step 17 and half of 18

---
+++ Makestagram/Makestagram/Makestagram/PostActionCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostActionCell.swift
+//  Makestagram
+//
+//  Created by Lauren Weiss on 6/29/17.
+//  Copyright © 2017 Lauren Weiss. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostActionCell: UITableViewCell{
+    
+    // MARK: - Subviews
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var timeAgoLabel: UILabel!
+    @IBOutlet weak var likeCountLabel: UILabel!
+    
+    // MARK: - Cell Lifecycle
+    
+    override func awakeFromNib() {
+            super.awakeFromNib()
+        }
+    // MARK: - IBActions
+    
+    @IBAction func likeButtonTapped(_ sender: UIButton) {
+        print("Like button tapped")
+    }
+}
---
+++ Makestagram/Makestagram/Makestagram/PostHeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostHeaderCell.swift
+//  Makestagram
+//
+//  Created by Lauren Weiss on 6/29/17.
+//  Copyright © 2017 Lauren Weiss. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostHeaderCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    @IBAction func optionsButtonTapped(_ sender: UIButton) {
+        print("options button tapped")
+    }
+}
